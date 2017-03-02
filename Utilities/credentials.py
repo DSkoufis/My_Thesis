f546d4c7ca4c69ed58513c7bf8c19c2a298bff19
Dimitris_Skoufis <skoufisdimitris@gmail.com>
2017-03-02 22:36:34 +0200

    init

---
+++ Utilities/credentials.py
@@ -0,0 +1,44 @@
+# ----------------------------
+# This module is to handle the credentials from the credentials.txt
+# ----------------------------
+
+# This method reads the credentials of the credentials.txt and returns a dictionary with them
+def read():
+    try:
+        credentials = {} # we will return this dictionary
+        path = "../credentials.txt" # path to our file. It is in the parent directory by default
+        with open(path, "r") as creds:
+            content = creds.readlines()  # this holds all credentials into a long list
+
+            # this line deletes the line break character from the list
+            content = [line.strip("\n") for line in open(path)]
+
+            print ("Reading credentials")
+    # handling the errors
+    except IOError as e:
+        print ("I/O error({0}): {1}".format(e.errno, e.strerror))
+        return None
+    except:
+        print("Error on reading file")
+        return None
+    print("Done reading, starting to converting into dictionary")
+
+    i = 0
+    while i <= 3:
+        line = content[i] # getting the ith element of the list
+        line_i = line.split(":") # split the content into a smaller list
+        # every line is in name:tokens format
+        credentials[line_i[0]] = line_i[1] # from the smaller list, add the 1th element into the
+        # into the dictionary with the key of the 0th element
+        i += 1
+
+    return credentials # return the dictionary
+
+# this method returns a tuple with the formal names of the credentials.txt fields
+def set_names():
+    names = ("consumer_key",
+             "consumer_secret",
+             "access_token",
+             "access_token_secret",)
+    return names
+
