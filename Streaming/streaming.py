ea29e4abd096865138485a1754009558f9992de4
Dimitris_Skoufis <skoufisdimitris@gmail.com>
2017-03-05 16:53:46 +0200

    added client.py, responsible to make connection to twitter server

--- Streaming/streaming.py
+++ Streaming/streaming.py
@@ -1,16 +1,5 @@
 from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
-from Utilities import credentials
-
-creds = credentials.read()  # this is a dictionary that holds the credentials
-creds_names = credentials.set_names()  # this is a tuple that holds some strings with formal names
-
-# we gather our credential tokens into these variable to authenticate with twitter API
-consumer_key = creds[creds_names[0]]
-consumer_secret = creds[creds_names[1]]
-access_token = creds[creds_names[2]]
-access_token_secret = creds[creds_names[3]]
+from Utilities import client
 
 
 # This is a basic listener that just prints received tweets to stdout.
@@ -23,15 +12,10 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print(status)
 
-# TODO: change this if statement
-if __name__ == "__main__":
 
-    # This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
+listener = StdOutListener()
+stream = client.set_stream(listener) # this is the srteam item, responsible to open the stream for us
 
-    # This line filter Twitter Streams to capture data by the given keywords
-    # TODO: change this by user's keyword
-    stream.filter(track=['basketball'])
+# This line filter Twitter Streams to capture data by the given keywords
+# TODO: change this by user's keyword
+stream.filter(track=['basketball'])
