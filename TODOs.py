7a5df7261f0dc2c88aa736715c461eba1f570748
Dimitris_Skoufis <skoufisdimitris@gmail.com>
2017-05-09 00:29:41 +0300

    [MODIFIED | UPDATED] Modified gitignore to ignore log file and updated the to-do list

--- TODOs.py
+++ TODOs.py
@@ -10,5 +10,3 @@
 # TODO: Make a window, which prints all tweets with the given keyword (user gives it)
 
 # TODO: Make stream and search log messages, printed on a toplevel window
-
-# TODO: Add logging in a log file and proper logging system
